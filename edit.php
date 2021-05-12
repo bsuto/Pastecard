ea3930de45bf3029b42773f5a872c0acab28d25c
Brian Sutorius <brian.sutorius@gmail.com>
2021-05-12 14:13:31 -0400

    better php decoding

--- edit.php
+++ edit.php
@@ -2,10 +2,6 @@
 // take in the input from the textarea
 $pcOut = $_POST['card'];
 
-// URL-decode it and strip out the slashes
-$pcOut = rawurldecode($pcOut);
-$pcOut = str_replace('\\', '', $pcOut);
-
 // open the text file and replace its contents with the textarea's
 $filename = 'output.txt';
 $handle = fopen($filename, 'w');
